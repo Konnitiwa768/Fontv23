0d9c7fcd5b7aa52af4e9b75aee1af7558ac4a18f
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-07-15 16:24:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI, Form, Request
-from fastapi.responses import FileResponse, HTMLResponse, JSONResponse
+from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import os, base64, io, time
-from PIL import Image
 from fontTools.ttLib import TTFont, newTable
 from fontTools.pens.ttGlyphPen import TTGlyphPen
-from fontTools.pens.svgPathPen import SVGPathPen
-from fontTools.svgLib.path import parse_path
-import requests
+from PIL import Image
+import potrace
+import os, base64, io, time
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -32,83 +30,61 @@ async def save_glyph(char: str = Form(...), image: str = Form(...)):
 def build_font():
     font = TTFont()
     font.setGlyphOrder([".notdef"])
-    glyphs = font.getGlyphSet()
-    pen = TTGlyphPen(glyphs)
-
-    glyf_table = newTable('glyf')
+    glyph_order = [".notdef"]
+    glyf_table = newTable("glyf")
     hmtx = {}
     cmap = {}
-    glyph_order = [".notdef"]
+    glyphs = font.getGlyphSet()
 
     for fname in os.listdir("glyphs"):
-        if not fname.endswith(".png"):
-            continue
+        if not fname.endswith(".png"): continue
         codepoint = int(fname.split(".")[0])
-        img = Image.open(f"glyphs/{fname}").convert("1").resize((1000, 1000))
-        img_data = img.tobytes()
+        img = Image.open(f"glyphs/{fname}").convert("L").resize((1000, 1000))
+        bw = img.point(lambda x: 0 if x < 128 else 255, "1")
 
+        bitmap = []
+        pixels = bw.load()
+        for y in range(bw.height):
+            row = []
+            for x in range(bw.width):
+                row.append(1 if pixels[x, y] == 0 else 0)
+            bitmap.append(row)
+
+        bmp = potrace.Bitmap(bitmap)
+        path = bmp.trace()
         pen = TTGlyphPen(glyphs)
-        for y in range(1000):
-            for x in range(1000):
-                if img.getpixel((x, y)) == 0:
-                    pen.moveTo((x, 1000 - y))
-                    pen.lineTo((x + 1, 1000 - y))
-                    pen.lineTo((x + 1, 999 - y))
-                    pen.lineTo((x, 999 - y))
-                    pen.closePath()
-        glyph = pen.glyph()
-        name = f"uni{codepoint:04X}"
-        glyf_table[name] = glyph
-        hmtx[name] = (1000, 0)
-        cmap[codepoint] = name
-        glyph_order.append(name)
 
+        for curve in path:
+            start = curve.start_point
+            pen.moveTo(start)
+            for segment in curve:
+                if segment.is_corner:
+                    pen.lineTo(segment.c)
+                else:
+                    pen.qCurveTo(segment.c1, segment.c2)
+            pen.closePath()
+
+        glyph_name = f"uni{codepoint:04X}"
+        glyph_order.append(glyph_name)
+        glyf_table[glyph_name] = pen.glyph()
+        hmtx[glyph_name] = (1000, 0)
+        cmap[codepoint] = glyph_name
+
+    # テーブル構成
     font["glyf"] = glyf_table
-    font["hmtx"] = newTable("hmtx")
-    font["hmtx"].metrics = hmtx
+    font["hmtx"] = newTable("hmtx"); font["hmtx"].metrics = hmtx
     font["cmap"] = newTable("cmap")
-    font["cmap"].tableVersion = 0
-    font["cmap"].tables = []
     from fontTools.ttLib.tables._c_m_a_p import CmapSubtable
-    cmap_table = CmapSubtable.newSubtable(4)
-    cmap_table.platformID = 3
-    cmap_table.platEncID = 1
-    cmap_table.language = 0
-    cmap_table.cmap = cmap
-    font["cmap"].tables.append(cmap_table)
-
-    font["head"] = newTable("head")
-    font["head"].unitsPerEm = 1000
-    font["maxp"] = newTable("maxp")
-    font["maxp"].numGlyphs = len(glyph_order)
-    font["name"] = newTable("name")
-    font["name"].names = []
-    font["post"] = newTable("post")
-    font["post"].formatType = 3.0
+    subtable = CmapSubtable.newSubtable(4)
+    subtable.platformID = 3
+    subtable.platEncID = 1
+    subtable.language = 0
+    subtable.cmap = cmap
+    font["cmap"].tables = [subtable]
+    font["head"] = newTable("head"); font["head"].unitsPerEm = 1000
+    font["maxp"] = newTable("maxp"); font["maxp"].numGlyphs = len(glyph_order)
+    font["name"] = newTable("name"); font["name"].names = []
+    font["post"] = newTable("post"); font["post"].formatType = 3.0
     font.setGlyphOrder(glyph_order)
-
     font.save("myfont.ttf")
-    return FileResponse("myfont.ttf", media_type="application/x-font-ttf", filename="myfont.ttf")
-
-@app.post("/upload_font")
-def upload_font():
-    GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
-    REPO = "Konnitiwa768/Fontv23"  # 変更！
-    PATH = f"fonts/myfont-{int(time.time())}.ttf"
-    url = f"https://api.github.com/repos/{REPO}/contents/{PATH}"
-
-    with open("myfont.ttf", "rb") as f:
-        content = base64.b64encode(f.read()).decode("utf-8")
-
-    res = requests.put(url, headers={
-        "Authorization": f"token {GITHUB_TOKEN}",
-        "Accept": "application/vnd.github.v3+json"
-    }, json={
-        "message": "upload font",
-        "content": content
-    })
-
-    try:
-        return {"url": res.json()["content"]["html_url"]}
-    except:
-        return {"error": res.text}
+    return FileResponse("myfont.ttf", media_type="font/ttf", filename="myfont.ttf")
